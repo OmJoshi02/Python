b1e0090005fb898d6559de4a921947f1a71d256a
OmJoshi02 <omj020705@gmail.com>
2025-08-21 15:27:00 +0530

    Delete Unit 1 directory

--- Unit 1/Functions/default.py
+++
@@ -1,5 +0,0 @@
-def default(a, b = 10):
-    return a, b
-
-if __name__ == "__main__":
-    print(default(20))
--- Unit 1/Functions/normal.py
+++
@@ -1,9 +0,0 @@
-def name1():
-    print("Hello")
-
-def name2():
-    print("World")
-
-if __name__ == "__main__":
-    name1()
-    name2()
