5ab95ba7cb7e23c812e05f6e099a03dfc1e5863b
OmJoshi02 <omj020705@gmail.com>
2025-09-16 23:01:55 +0530

    Delete Practice programs directory

--- Practice programs/prog1.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Enter the value : "))
-
-if num == 0:
-    print("Number entered is 0")
-
-else:
-    print(f"Number entered is {num}")
--- Practice programs/prog2.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Enter number 1 : "))
-num2 = int(input("Enter number 2 : "))
-
-if num1 > num2 :
-    print(f"The {num1} is greater than {num2}")
-
-else:
-    print(f"The {num2} is greater than {num1}")
